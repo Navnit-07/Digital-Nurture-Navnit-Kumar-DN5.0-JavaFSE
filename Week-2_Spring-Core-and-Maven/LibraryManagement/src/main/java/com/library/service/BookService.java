d01f39cd696a286c54bee71ec92c3f73f0f8aa45
Navnak <navnit.23bce11827@vitbhopal.ac.in>
2026-07-04 16:09:57 +0530

    add Country REST endpoints with XML bean loading

--- Week-2_Spring-Core-and-Maven/LibraryManagement/src/main/java/com/library/service/BookService.java
+++ Week-2_Spring-Core-and-Maven/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -8,20 +8,8 @@ public class BookService {
 
     private BookRepository bookRepository;
 
-    // Default constructor
-    public BookService() {
-        System.out.println("BookService: default constructor called");
-    }
-
-    // Constructor injection
-    public BookService(BookRepository bookRepository) {
-        System.out.println("BookService: constructor injection called");
-        this.bookRepository = bookRepository;
-    }
-
-    // Setter injection
+    // Setter for dependency injection
     public void setBookRepository(BookRepository bookRepository) {
-        System.out.println("BookService: setter injection called");
         this.bookRepository = bookRepository;
     }
 
